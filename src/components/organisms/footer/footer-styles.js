e197863932e07c96043bfb8e3024d70745022122
Mateusz Grzejszczyk <mateuszgrzejszczyk1@gmail.com>
2020-12-20 12:29:08 +0100

    fixing footer error2

--- src/components/organisms/footer/footer-styles.js
+++ src/components/organisms/footer/footer-styles.js
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 
 export const Wrapper = styled.nav`
    display: flex;
-   width: 100%;
    height: 20vh;
    background-color: rgb(239, 239, 239);
    color: black;
