d866566d34d1023d881ed1bf9cdf9a577bf52444
Mateusz Grzejszczyk <mateuszgrzejszczyk1@gmail.com>
2020-12-20 12:34:41 +0100

    fixing error footer3

--- src/components/organisms/footer/footer-styles.js
+++ src/components/organisms/footer/footer-styles.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const Wrapper = styled.nav`
    display: flex;
    height: 20vh;
+   max-width:100vw;
    background-color: rgb(239, 239, 239);
    color: black;
    flex-direction: row;
